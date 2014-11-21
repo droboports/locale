74aa597b48f73414656ad85a2ca03e16ba90b013
Ricardo Padilha <ricardospadilha@gmail.com>
2014-11-21 10:35:11 +0100

    First version

---
+++ build.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+### bash best practices ###
+# exit on error code
+set -o errexit
+# exit on unset variable
+set -o nounset
+# return error of last failed command in pipe
+set -o pipefail
+# expand aliases
+shopt -s expand_aliases
+# print trace
+set -o xtrace
+
+### logfile ###
+timestamp="$(date +%Y-%m-%d_%H-%M-%S)"
+logfile="logfile_${timestamp}.txt"
+echo "${0} ${@}" > "${logfile}"
+# save stdout to logfile
+exec 1> >(tee -a "${logfile}")
+# redirect errors to stdout
+exec 2> >(tee -a "${logfile}" >&2)
+
+### environment variables ###
+source crosscompile.sh
+export NAME="locale"
+export DEST="/mnt/DroboFS/Shares/DroboApps/${NAME}"
+export DEPS="/mnt/DroboFS/Shares/DroboApps/${NAME}deps"
+export CFLAGS="$CFLAGS -Os -fPIC"
+export CXXFLAGS="$CXXFLAGS $CFLAGS"
+export CPPFLAGS="-I${DEPS}/include"
+export LDFLAGS="${LDFLAGS:-} -Wl,-rpath,${DEST}/lib -L${DEST}/lib"
+alias make="make -j8 V=1 VERBOSE=1"
+
+# $1: file
+# $2: url
+# $3: folder
+_download_tgz() {
+  [[ ! -f "download/${1}" ]] && wget -O "download/${1}" "${2}"
+  [[ -d "target/${3}" ]] && rm -v -fr "target/${3}"
+  [[ ! -d "target/${3}" ]] && tar -zxvf "download/${1}" -C target
+  return 0
+}
+
+# $1: file
+# $2: url
+# $3: folder
+_download_app() {
+  [[ ! -f "download/${1}" ]] && wget -O "download/${1}" "${2}"
+  [[ -d "target/${3}" ]] && rm -v -fr "target/${3}"
+  mkdir -p "target/${3}"
+  tar -zxvf "download/${1}" -C target/${3}
+  return 0
+}
+
+# $1: branch
+# $2: folder
+# $3: url
+_download_git() {
+  [[ -d "target/${2}" ]] && rm -v -fr "target/${2}"
+  [[ ! -d "target/${2}" ]] && git clone --branch "${1}" --single-branch --depth 1 "${3}" "target/${2}"
+  return 0
+}
+
+# $1: file
+# $2: url
+_download_file() {
+  [[ ! -f "download/${1}" ]] && wget -O "download/${1}" "${2}"
+  return 0
+}
+
+### LOCALE ###
+_build_locale() {
+mkdir -p "${DEST}/bin" "${DEST}/usr/share" "${DEST}/usr/lib/locale"
+cp "${TOOLCHAIN}/arm-marvell-linux-gnueabi/libc/usr/bin/localedef" "${DEST}/bin/"
+cp "${TOOLCHAIN}/arm-marvell-linux-gnueabi/libc/usr/bin/locale" "${DEST}/bin/"
+cp -R "${TOOLCHAIN}/arm-marvell-linux-gnueabi/libc/usr/share/i18n" "${DEST}/usr/share/"
+}
+
+### BUILD ###
+_build() {
+  _build_locale
+  _package
+}
+
+_create_tgz() {
+  local appname="$(basename ${PWD})"
+  local appfile="${PWD}/${appname}.tgz"
+
+  if [[ -f "${appfile}" ]]; then
+    rm -v "${appfile}"
+  fi
+
+  pushd "${DEST}"
+  tar --verbose --create --numeric-owner --owner=0 --group=0 --gzip --file "${appfile}" *
+  popd
+}
+
+_package() {
+  mkdir -p "${DEST}"
+  cp -avfR src/dest/* "${DEST}"/
+  find "${DEST}" -name "._*" -print -delete
+  _create_tgz
+}
+
+_clean() {
+  rm -v -fr "${DEPS}"
+  rm -v -fr "${DEST}"
+  rm -v -fr target/*
+}
+
+_dist_clean() {
+  _clean
+  rm -v -f logfile*
+  rm -v -fr download/*
+}
+
+case "${1:-}" in
+  clean)     _clean ;;
+  distclean) _dist_clean ;;
+  package)   _package ;;
+  "")        _build ;;
+  *)         _build_${1} ;;
+esac
---
+++ src/dest/service.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env sh
+#
+# Locale support
+
+# import DroboApps framework functions
+. /etc/service.subr
+
+# DroboApp framework version
+framework_version="2.0"
+
+# app description
+name="locale"
+version="2.5"
+description="Support for filenames with international characters."
+
+# framework-mandated variables
+pidfile="/tmp/DroboApps/${name}/pid.txt"
+logfile="/tmp/DroboApps/${name}/log.txt"
+statusfile="/tmp/DroboApps/${name}/status.txt"
+errorfile="/tmp/DroboApps/${name}/error.txt"
+
+# app-specific variables
+prog_dir=$(dirname $(readlink -fn ${0}))
+inputfile="en_US"
+charmap="UTF-8"
+
+# script hardening
+set -o errexit  # exit on uncaught error code
+set -o nounset  # exit on unset variable
+set -o pipefail # propagate last error code on pipe
+
+# ensure log folder exists
+logfolder="$(dirname ${logfile})"
+if [[ ! -d "${logfolder}" ]]; then mkdir -p "${logfolder}"; fi
+
+# redirect all output to logfile
+exec 3>&1 1>> "${logfile}" 2>&1
+
+# log current date, time, and invocation parameters
+echo $(date +"%Y-%m-%d %H-%M-%S"): ${0} ${@}
+
+# enable script tracing
+set -o xtrace
+
+if_mkdir() {
+  if [ ! -d "$1" ]; then mkdir -p "$1"; fi
+}
+
+if_link() {
+  if [ ! -h "$1" ]; then ln -fs "$2" "$1"; fi
+}
+
+if_create_locale() {
+  local localecount
+  localecount=$("${prog_dir}/bin/locale" -a | wc -l)
+  if [[ ${localecount} -le 2 ]]; then
+    "${prog_dir}/bin/localedef" -f "${charmap}" -i "${inputfile}" "${inputfile}.${charmap}"
+  fi
+}
+
+start() {
+  if_mkdir "/usr/share"
+  if_mkdir "/usr/lib"
+  if_link "/usr/share/i18n" "${prog_dir}/usr/share/i18n"
+  if_link "/usr/lib/locale" "${prog_dir}/usr/lib/locale"
+  if_create_locale
+}
+
+status() {
+  local enabled running localecount
+
+  if [[ -h "/usr/lib/locale" ]]; then
+    enabled="enabled"
+  else
+    enabled="disabled"
+  fi
+
+  localecount=$("${prog_dir}/bin/locale" -a | wc -l)
+  if [[ "${localecount}" -le 2 ]]; then
+    running="running"
+  else
+    running="stopped"
+  fi
+
+  echo ${name} is ${enabled} and ${running}
+}
+
+case "$1" in
+  start)   start ;;
+  stop)    ;;
+  restart) start ;;
+  status)  status ;;
+  *) echo "Usage: $0 [start|stop|restart|status]" ; exit 1 ;;
+esac
